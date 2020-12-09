61b82773cedc3f0878199ee9659744dcf56de064
jaxonl <lin.jiaxuan@hotmail.com>
2020-12-09 00:28:29 -0500

    brute force day 8 part 2
    computation time too long for part 2 on actual input/may have bug

--- 2020/8/handheldHalting.py
+++ 2020/8/handheldHalting.py
@@ -44,11 +44,75 @@ def getAccValueBeforeLoop(ops):
 
     return acc
 
+def changeInstructionAndGetFinalAcc(ops):
+    # maybe go forward til last instruction before visited, and keep a stack of previous instructions
+    # if looped, pop previous instruction
+    #   if prev instr = nop/jmp, switch, and check if continuing does anything
+    #       save index that was changed
+    acc = 0
+    visited = [0] * len(ops)
+    viewingQueue = []
+    i = 0
+    changedIndex = None
+    originalOpVal = None
+    while i < len(ops):
+        # print('executing', i, ops[i], end=': ')
+        if visited[i] is 1:
+            # start bactracking
+            # has seen -- shouldn't error else something extremely wrong
+            prevIndex = viewingQueue.pop()
+            # do some things
+            prevOp, prevVal = ops[prevIndex]
+            # print(prevOp, prevVal)
+            while prevOp == 'acc' or (prevOp == 'nop' and prevVal == 0) or (prevIndex == changedIndex):
+                # print('\n\tremoving instruction', prevIndex, ' - ', prevOp, prevVal, end=';')
+                if prevIndex == changedIndex:
+                    # change it back
+                    ops[changedIndex] = originalOpVal
+                    changedIndex = None
+                    originalOpVal = None
+                else:
+                    # remove acc addition
+                    acc -= prevVal
+                # print(' new acc', acc)
+                visited[prevIndex] = 0
+                prevIndex = viewingQueue.pop()
+                prevOp, prevVal = ops[prevIndex]
+
+            if prevOp == 'nop':
+                # change to jmp
+                changedIndex = prevIndex
+                originalOpVal = ops[changedIndex]
+                ops[changedIndex] = ('jmp', prevVal)
+                i, acc = JMP(prevIndex, acc, prevVal)
+                # print('\n\tchanged', prevIndex, 'to jmp from', prevOp, prevVal, 'acc is', acc)
+            else:
+                # is a jmp, change to nop
+                changedIndex = prevIndex
+                originalOpVal = ops[changedIndex]
+                ops[changedIndex] = ('nop', prevVal)
+                i, acc = NOP(prevIndex, acc, prevVal)
+                # print('\n\tchanged', prevIndex, 'to nop from', prevOp, prevVal, 'acc is', acc)
+
+            viewingQueue.append(prevIndex)
+        else:
+            # have not seen before, continue
+            op, val = ops[i]
+            visited[i] = 1
+            viewingQueue.append(i)
+            i, acc = OP_DICT.get(op)(i, acc, val)
+            # print('acc is', acc)
+        # print('\t viewingQueue:', viewingQueue)
+
+    # print('changed instr at index', changedIndex, ops[changedIndex])
+    return acc
+
 def dayEight():
     # instructions = readList('d8-sample.in', getOpValPair)
     instructions = readList('d8.in', getOpValPair)
     # for x in instructions[:5]:
     #     print(x)
-    print(getAccValueBeforeLoop(instructions))
+    # print('part one:', getAccValueBeforeLoop(instructions))
+    print('part two:', changeInstructionAndGetFinalAcc(instructions))
 
 dayEight()
